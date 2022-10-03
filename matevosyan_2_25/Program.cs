36b00922755675f4b4e556718c409f2a011ecf3d
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2022-10-03 12:11:02 +0300

    Add files via upload

---
+++ matevosyan_2_25/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace matevosyan_2_25
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter n");
+            int n = int.Parse(Console.ReadLine());
+
+            int firstSymbol = n / 100;
+
+            int otherSymbols = n - firstSymbol * 100;
+
+            int x = otherSymbols * 10 + firstSymbol;
+
+            Console.WriteLine(x);
+        }
+    }
+}
+    
